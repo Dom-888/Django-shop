a3e7f2b6341f20b4b1b7a10b72eea495f8935dea
Dom-888 <spinelladomenico88@gmail.com>
2020-07-03 19:59:11 +0000

    Add prduct list

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+# Return a view of all products
+def all_products(request):
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context) 
