60da2493689bbbda824947b388eeb7bc2b57322d
Dom-888 <spinelladomenico88@gmail.com>
2020-07-07 13:48:32 +0000

    Add category filter

--- products/views.py
+++ products/views.py
@@ -1,44 +1,48 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
-from django.db.models import Q # Generate a search query
-from .models import Product
+from django.db.models import Q
+from .models import Product, Category
 
 # Create your views here.
 
-# Return a view of all products
 def all_products(request):
+    """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
-
-    context = {
-        'products': products,
-    }
-
-    return render(request, 'products/products.html', context) 
-
-# Return the details of the selected product
-def product_detail(request, product_id):
-    product = get_object_or_404(Product, pk=product_id)
-
-    # Handle the search query
     query = None
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
-            # Se la query è vuota
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
-
-            # Cerca sia nel nome del prodotto, sia nella descrizione
+            
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
     context = {
-        'product': product,
-        'search_term': query
+        'products': products,
+        'search_term': query,
+        'current_categories': categories,
     }
 
-    return render(request, 'products/product_detail.html', context) 
+    return render(request, 'products/products.html', context)
 
+
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
