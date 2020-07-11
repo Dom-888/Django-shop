e3b05a49b7830f3261372df30099729974e4f5ff
Dom-888 <spinelladomenico88@gmail.com>
2020-07-11 18:28:02 +0000

    Add shopping bag app

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Questa è la base da cui partire per creare le views delle varie apps, passando nel secondo argomento my_app/my_app.html
 
 def index(request):
     return render(request, 'home/index.html') # Return the index page
+
