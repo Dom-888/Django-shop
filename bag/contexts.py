a6749ebd4a25f53991ca6641a3c26e862530a17c
Dom-888 <spinelladomenico88@gmail.com>
2020-07-15 14:21:58 +0000

    Add product size logic to bag app

--- bag/contexts.py
+++ bag/contexts.py
@@ -14,16 +14,33 @@ def bag_contents(request):
     product_count = 0
     bag = request.session.get('bag', {})
 
-    for item_id, quantity in bag.items(): # Questa è la bag from the session
-        product = get_object_or_404(Product, pk=item_id) # pk sta per Primary Key
-        total += quantity * product.price
-        product_count += quantity
-        # Dizionario del prodotto da aggiungere/aggiornare
-        bag_items.append({
-            'item_id':item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+    # item_data = nel caso di un oggetto senza taglia, è uguale al numero di oggetti,
+    # nel caso di un oggetto con taglia, è un dizionario di tutti gli oggetti e la loro taglia
+
+    for item_id, item_data in bag.items(): # Questa è la bag from the session
+        if isinstance(item_data, int): # Esegue il codice sottostante solo se per gli oggetti senza taglia, in pratica controlla che item_data sia un integrale (e non un dict)
+            product = get_object_or_404(Product, pk=item_id) # pk sta per Primary Key
+            total += item_data * product.price
+            product_count += item_data
+            # Dict del prodotto da aggiungere/aggiornare
+            bag_items.append({
+                'item_id':item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        else: # Items Con taglia
+            product = get_object_or_404(Product, pk=item_id) # pk sta per Primary Key
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                # Dict del prodotto da aggiungere/aggiornare
+                bag_items.append({
+                    'item_id':item_id,
+                    'quantity': item_data,
+                    'product': product,
+                    'size': size,
+                })
+
 
     # Se il totale è inferiore alla soglia, le spedizioni sono uguali al 10% del totale
     if total < settings.FREE_DELIVERY_THREESHOLD:
--- bag/views.py
+++ bag/views.py
@@ -2,23 +2,40 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 
-def views_bag(request):
+def view_bag(request):
     return render(request, 'bag/bag.html') 
 
-# Raccoglie dati products > product_detail.html
+# Raccoglie dati products/product_detail.html
 def add_to_bag(request, item_id):
     quantity = int(request.POST.get('quantity'))
 
     # Necessario per ritornare alla stessa pagina una volta che l'oggetto è stato inserito nel carrello
     redirect_url = request.POST.get('redirect_url')
 
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+
     # Crea una session variable per conservare le informazioni del carrello
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
-    else:
-        bag[item_id] = quantity
+    if size:
+        # Se l'oggetto è già nella bag, controlla se c'è già in quella misura e ne modifica la quantità
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+            else: 
+                # Se l'oggetto esiste già ma ha una taglia diversa
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            # Se l'oggetto non è nella bag, viene semplicemente aggiunto
+            bag[item_id] = {'items_by_size': {size: quantity}}
+
+    else: # No size
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
     print(request.session['bag'])
